d9b38192a6c6f10c6be5243233a1d8d5c5bf0492
linzetian <65435639+linzetian@users.noreply.github.com>
2020-12-24 17:59:42 +0800

    Add files via upload

---
+++ 2020-12-24-3利用malloc开辟内存空间。.c
@@ -0,0 +1,24 @@
+//利用malloc开辟内存空间
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	int* p = (int*)malloc(10 * sizeof(int));//10 * sizeof(int)//换为INT_MAX
+	if (p == NULL)
+	{
+		printf("%s",strerror(errno));
+	}
+	else
+	{
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			*(p + i) = i;
+		}
+		int j;
+		for(j=0;j<10;j++)
+		printf("%d ",*(p+j));
+	}
+	return 0;
+}
